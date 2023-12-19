7292d50b768b0c70b4bee0fe438f2cb08fed94ec
xssh99 <145438188+xssh99@users.noreply.github.com>
2023-12-20 02:02:37 +0800

    Update bbr.sh

--- linak/bbr.sh
+++ linak/bbr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Edition : Stable Edition V3.0
-# Auther  : Geo Project
+# Auther  : Julak Bantur
 # (C) Copyright 2023
 # =========================================
 
@@ -18,7 +18,7 @@ clear
 #	 menu
 #else
 
-echo -e "Installing TCP BBR Mod By Lunatic"
+echo -e "Installing TCP BBR Mod By Julak Bantur"
 echo -e "Please Wait BBR Installation Will Starting . . ."
 sleep 5
 clear
